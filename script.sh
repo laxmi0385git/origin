3f1ee4fec19dd04d9c5e7e528342b757b19f14ab
Lakshmi Narayana <nlakshmi@Lucky.com>
2019-11-06 13:06:54 +0000

    Added my first script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+########################################
+#Purpose: To Learn Shell Scripting
+#Input: none
+#Output: Print Some messages on screen
+#Owner: Lakshmi Narayana <lakshminarayana@mycompany.com>
+#version: 1.0
+#######################################
+#print a message
+echo 'Welcome to Shell Scripting'
+sleep 10
+#print end message
+echo 'End'
